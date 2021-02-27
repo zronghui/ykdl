cdbb8d65baf88e4ad21bbd12a9eacf52f27b34de
zronghui <825503975@qq.com>
2021-02-27 10:35:47 +0800

    增添录制的主播

--- crontab.sh
+++ crontab.sh
@@ -17,10 +17,10 @@ watchLive() {
 	sleep 5
 }
 
-
+watchLive "https://live.bilibili.com/21752762" "芝恩㱏"
+watchLive "https://live.bilibili.com/8857387" "老实憨厚的苏宇"
 watchLive "https://live.bilibili.com/22528847" "尤奶学姐"
 watchLive "https://live.bilibili.com/21753173" "搬砖小海绵"
-watchLive "https://live.bilibili.com/8857387" "老实憨厚的苏宇"
 watchLive "https://live.bilibili.com/22508500" "小妖晶软糖"
 watchLive "https://live.bilibili.com/10556121" "丸子うの"
 watchLive "https://live.bilibili.com/13328782" "圆圆小石头"
